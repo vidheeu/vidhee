5153df77be73e56dc47feaf131a14fa01b6bdadf
vidheeu <42140289+vidheeu@users.noreply.github.com>
2018-08-17 10:53:15 +0530

    Add files via upload

---
+++ lab_q1.cpp
@@ -0,0 +1,20 @@
+//using the library
+#include<iostream>
+using namespace std;
+//write the main function
+int main ()
+{
+//declaring the variables
+int a;
+float b;
+char c;
+bool d;
+double e;
+//print the results
+cout << "size of int is " << sizeof(a) << endl;
+cout << "size of float is " << sizeof(b) << endl;
+cout << "size of char is "<< sizeof(c) << endl;
+cout << "size of bool is "<< sizeof(d) << endl;
+cout << "size of double is "<< sizeof(e) << endl;
+return 0;
+}
